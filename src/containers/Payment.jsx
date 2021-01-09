62e1110f126697e3babfe6e13d9a5ca3dbdca741
Victor Casas <victor.casas@outlook.cl>
2021-01-09 13:45:10 -0300

    Layout payment flow  and styles

--- src/containers/Payment.jsx
+++ src/containers/Payment.jsx
@@ -1,3 +1,13 @@
-const Payment = () => <h1>Payment</h1>
+import '../styles/components/Payment.css'
+
+const Payment = () => (
+  <div className='Payment'>
+    <div className='Payment-content'>
+      <h3>Resument del pedido:</h3>
+      <div className='Payment-button'>Boton de pago con Paypal</div>
+    </div>
+    <div />
+  </div>
+)
 
 export default Payment
--- src/containers/Success.jsx
+++ src/containers/Success.jsx
@@ -1,3 +1,13 @@
-const Success = () => <h1>Success</h1>
+import '../styles/components/Success.css'
+
+const Success = () => (
+  <div className='Success'>
+    <div className='Success-content'>
+      <h2>Carlos, Gracias por tu compra</h2>
+      <span>Tu pedido lelgara en 3 dias a tu direccion:</span>
+      <div className='Success-map'>Google Maps</div>
+    </div>
+  </div>
+)
 
 export default Success
