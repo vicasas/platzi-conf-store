17033c1f19f6f49094ac4382d291810b71626d53
Victor Casas <victor.casas@outlook.cl>
2021-01-09 13:36:37 -0300

    Layout checkout and styles

--- src/containers/Checkout.jsx
+++ src/containers/Checkout.jsx
@@ -1,3 +1,22 @@
-const Checkout = () => <h1>Checkout</h1>
+import '../styles/components/Checkout.css'
+
+const Checkout = () => (
+  <div className='Checkout'>
+    <div className='Checkout-content'>
+      <h3>Lista de Pedidos:</h3>
+      <div className='Checkout-item'>
+        <div className='Checkout-element'>
+          <h4>ITEM name</h4>
+          <span>$10</span>
+        </div>
+        <button type='button'>Eliminar</button>
+      </div>
+    </div>
+    <div className='Checkout-sidebar'>
+      <h3>Precio Total: $10</h3>
+      <button type='button'>Continuar pedido</button>
+    </div>
+  </div>
+)
 
 export default Checkout
